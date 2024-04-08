f1d1eff637e843f569d6b5c63bfcfaadeb4577de
arver92 <arver92@gmail.com>
2024-04-08 10:52:21 -0400

    Add quiz brain functionality

--- QuizProject/main.py
+++ QuizProject/main.py
@@ -1,6 +1,12 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
-for new_q in question_data:
-    question_bank.append(Question(text=new_q["text"],answer=new_q["answer"]))
+for question in question_data:
+    question_bank.append(Question(text=question["text"],answer=question["answer"]))
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
