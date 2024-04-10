c2dbbc4e9f6d0605956338cde51f6948d1ccec73
arver92 <arver92@gmail.com>
2024-04-10 14:47:16 -0400

    Complete Spirograph program

--- TurtleProject/Spirograph.py
+++ TurtleProject/Spirograph.py
@@ -2,20 +2,19 @@ import turtle as t
 import random
 
 t.colormode(255)
+
 tim = t.Turtle()
+tim.speed("fastest")
 
 def get_random_color():
     # Returns a tuple representing an RGB color, where each color component (Red, Green, Blue) 
     # is a random integer between 0 and 255, inclusive.
     return (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
-for number_sides in range(3,11):
-    turn_angle = 360/number_sides
-    edge_length = 50
+for turn_angle in range(0,361,10):
+    tim.circle(100)
     tim.color(get_random_color())
-    for _ in range(number_sides):
-        tim.forward(edge_length)
-        tim.right(turn_angle)
-
+    tim.setheading(turn_angle)
+    
 screen = t.Screen()
 screen.exitonclick()
