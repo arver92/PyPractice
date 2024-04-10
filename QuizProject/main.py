98f03287d9d934777d06d755b0b51a95fc38b3aa
arver92 <arver92@gmail.com>
2024-04-10 09:52:34 -0400

    Complete Quiz Project

--- QuizProject/main.py
+++ QuizProject/main.py
@@ -4,9 +4,12 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_bank.append(Question(text=question["text"],answer=question["answer"]))
+    question_bank.append(Question(text=question["question"],answer=question["correct_answer"]))
 
 quiz = QuizBrain(question_bank)
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+print("You've compelted the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
